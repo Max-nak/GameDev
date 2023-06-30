2e709297576720488333fdccf92b728fee9748c4
Max-nak <max_nak@mail.ru>
2023-06-30 15:31:23 +0300

    add error page

--- GameDev/src/App.jsx
+++ GameDev/src/App.jsx
@@ -1,6 +1,4 @@
 // import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Header from './components/header/Header.jsx'
 import Footer from './components/footer/Footer.jsx'
@@ -10,22 +8,24 @@ import Games from './components/pages/games/Games.jsx'
 import Career from './components/pages/career/Career.jsx'
 import News from './components/pages/news/News.jsx'
 import Contacts from './components/pages/contacts/Contacts.jsx'
+import ErrorPage from './components/pages/error/ErrorPage.jsx'
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <div>
       <Header />
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/company' element={<Company />} />
-          <Route path='/games' element={<Games />} />
-          <Route path='/career' element={<Career />} />
-          <Route path='/news' element={<News />} />
-          <Route path='/contacts' element={<Contacts />} />
+          <Route errorElement={<ErrorPage />} >
+            <Route path='/' element={<Home />} />
+            <Route path='/company' element={<Company />} />
+            <Route path='/games' element={<Games />} />
+            <Route path='/career' element={<Career />} />
+            <Route path='/news' element={<News />} />
+            <Route path='/contacts' element={<Contacts />} />
+          </Route>
         </Routes>
       </BrowserRouter>
       <Footer />
---
+++ GameDev/src/components/pages/error/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import './ErrorPage.css'
+
+function ErrorPage(){
+    return(
+        <div>
+            <h1>
+                Our games
+            </h1>
+        </div>
+    )
+}
+
+export default ErrorPage
